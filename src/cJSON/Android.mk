5b08426fadfcc57a52574b80d1620b667808b6c2
Steven Haid <stevenhaid@gmail.com>
2026-01-04 07:37:28 -0500

    updates to cJSON to build for Linux and Android

---
+++ src/cJSON/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cJSON
+
+LOCAL_SRC_FILES := cJSON.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(BUILD_STATIC_LIBRARY)
