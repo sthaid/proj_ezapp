342f2c4f678d45063731ce6a082be737c1e468e6
Steven Haid <stevenhaid@gmail.com>
2026-01-04 16:21:48 -0500

    updates to eztest

--- src/picoc/picoc.c
+++ src/picoc/picoc.c
@@ -11,12 +11,13 @@
     external interfaces, no internals from interpreter.h */
 #include "picoc.h"
 
-
 #if defined(UNIX_HOST) || defined(WIN32)
 #include "LICENSE.h"
 
 /* Override via STACKSIZE environment variable */
 #define PICOC_STACK_SIZE (128000*4)
+
+static char *progname;
 
 int main(int argc, char **argv)
 {
@@ -28,7 +29,8 @@ int main(int argc, char **argv)
     if (argc < 2 || strcmp(argv[ParamCount], "-h") == 0) {
         printf(PICOC_VERSION "  \n"
                "Format:\n\n"
-               "> picoc <file1.c>... [- <arg1>...]    : run a program, calls main() as the entry point\n"
+               "> picoc [-n <progname>] <file1.c>... [- <arg1>...]\n"
+               "                                      : run a program, calls main() as the entry point\n"
                "> picoc -s <file1.c>... [- <arg1>...] : run a script, runs the program without calling main()\n"
                "> picoc -i                            : interactive mode, Ctrl+d to exit\n"
                "> picoc -c                            : copyright info\n"
@@ -42,6 +44,13 @@ int main(int argc, char **argv)
     }
 
     PicocInitialize(&pc, StackSize);
+
+    // EZAPP added -n option to provide progname in argv[0], instead of "-"
+    if (strcmp(argv[ParamCount], "-n") == 0) {
+        ParamCount++;
+        progname = argv[ParamCount];
+        ParamCount++;
+    }
 
     if (strcmp(argv[ParamCount], "-s") == 0) {
         DontRunMain = true;
@@ -61,8 +70,15 @@ int main(int argc, char **argv)
         for (; ParamCount < argc && strcmp(argv[ParamCount], "-") != 0; ParamCount++)
             PicocPlatformScanFile(&pc, argv[ParamCount]);
 
-        if (!DontRunMain)
+        if (!DontRunMain) {
+            if (argc - ParamCount > 0 &&
+                strcmp(argv[ParamCount], "-") == 0 &&
+                progname != NULL)
+            {
+                argv[ParamCount] = progname;
+            }
             PicocCallMain(&pc, argc - ParamCount, &argv[ParamCount]);
+        }
     }
 
     PicocCleanup(&pc);
