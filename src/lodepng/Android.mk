08d5730f6865019166ec0553efe096729dd87327
Steven Haid <stevenhaid@gmail.com>
2026-01-04 07:34:46 -0500

    updates to lodepng Android.mk and CMakeLists.txt

---
+++ src/lodepng/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := lodepng
+
+LOCAL_SRC_FILES := lodepng.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/include
+
+include $(BUILD_STATIC_LIBRARY)
