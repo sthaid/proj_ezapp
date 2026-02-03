b92f529afb821ed66ed3ad7a7dad182fe390ffcc
Steven Haid <stevenhaid@gmail.com>
2026-02-03 08:41:10 -0500

    cont

--- files/svcs/Template/template.c
+++ files/svcs/Template/template.c
@@ -24,14 +24,17 @@ int main(int argc, char **argv)
     long abstime;
     int rc;
 
+    // set line buffering
+    setlinebuf(stdout);
+
     // save args
+    progname = argv[0];
     if (argc != 2) {
-        printf("ERROR: data_dir arg expected\n");
+        printf("E %s: data_dir arg expected\n", progname);
         return 1;
     }
-    progname = argv[0];
     data_dir = argv[1];
-    printf("INFO %s: starting, data_dir=%s\n", progname, data_dir);
+    printf("I %s: starting, data_dir=%s\n", progname, data_dir);
 
     // set absolute time at which svc_wait_for_req will timeout;
     // this time is rounded down to the prior minute so that the first
@@ -41,37 +44,37 @@ int main(int argc, char **argv)
     // service runtime loop
     while (!end_program) {
         // wait for req or timeout
-        rc = svc_wait_for_req(progname, &req, abstime); //xxx use abstime
+        rc = svc_wait_for_req(progname, &req, abstime);
 
         // if an unexpected error is returned, then delay and try again
         if (rc != 0 && rc != SVC_REQ_WAIT_ERROR_TIMEDOUT) {
-            printf("ERROR %s: svc_wait_for_req returned unexpected error %d\n", progname, rc);
+            printf("E %s: svc_wait_for_req returned unexpected error %d\n", progname, rc);
             sleep(1);
             continue;
         }
 
         // if scv_wait_for_req timedout then do periodic svc processing
         if (rc == SVC_REQ_WAIT_ERROR_TIMEDOUT) {
-            printf("INFO %s: do some processing\n", progname);
+            printf("I %s: do some processing\n", progname);
             abstime += 60;
             continue;
         }
 
         // if req was recvd then process the req
         if (req != NULL) {
-            printf("INFO %s: req=%p req->req_id=%d\n", progname, req, req->req_id);
+            printf("I %s: req=%p req->req_id=%d\n", progname, req, req->req_id);
             process_req(req);
         }
     }
             
     // print terminating msg
-    printf("INFO %s: terminating\n", progname);
+    printf("I %s: terminating\n", progname);
     return 0;
 }
 
 void process_req(svc_req_t *req)
 {
-    printf("INFO %s: got req_id %d\n", progname, req->req_id);
+    printf("I %s: got req_id %d\n", progname, req->req_id);
 
     // process the request
     switch (req->req_id) {
@@ -80,7 +83,7 @@ void process_req(svc_req_t *req)
         end_program = true;
         break;
     default:
-        printf("ERROR %s: req %d is invalid\n", progname, req->req_id);
+        printf("E %s: req %d is invalid\n", progname, req->req_id);
         svc_req_completed(req, SVC_REQ_ERROR_INVALID_REQ);
         break;
     }
