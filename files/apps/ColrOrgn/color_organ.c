08b99f60fce128862c4e2c4365ce8f845b25d217
Steven Haid <stevenhaid@gmail.com>
2026-03-17 09:03:43 -0400

    color organ

--- files/apps/ColrOrgn/color_organ.c
+++ files/apps/ColrOrgn/color_organ.c
@@ -49,7 +49,13 @@ sdlx_audio_state_t as;
 
 void register_events(void);
 void get_file_list(void);
-void display_color_organ(void);
+
+void color_organ_display(sdlx_audio_state_t *as);
+void color_organ_init(void);
+void color_organ_cleanup(void);
+void color_organ_register_events(void);
+void color_organ_process_event(sdlx_event_t *ev);
+
 void remove_trailing_newline(char *s);
 char *state_str(void);
 sdlx_texture_t *create_circle_texture(sdlx_color_t color);
@@ -75,10 +81,8 @@ int main(int argc, char **argv)
     y_controls = y_title + 1.5*sdlx_char_height_dflt;
     y_files_list = y_controls + 1.5*sdlx_char_height_dflt;
 
-    // init color organ red,green,blue circle textures
-    red_circle_texture   = create_circle_texture(COLOR_RED);
-    green_circle_texture = create_circle_texture(COLOR_GREEN);
-    blue_circle_texture  = create_circle_texture(COLOR_BLUE);
+    // xxx
+    color_organ_init();
 
     // runtime loop
     while (!end_program) {
@@ -94,11 +98,9 @@ int main(int argc, char **argv)
                                "%s", state_str());
 
         // display color organ
-        if (as.state != AUDIO_STATE_IDLE) {
-            display_color_organ();
-        }
+        color_organ_display(&as);
 
-        // display controls and register events
+        // register events
         register_events();
 
         // present the display
@@ -156,15 +158,18 @@ int main(int argc, char **argv)
             case EVID_QUIT:
                 end_program = true;
                 break;
+
+            // adjust color organ
+            default:
+                color_organ_process_event(&event);
+                break;
             }
         }
     }
 
     // cleanup
     sdlx_audio_stop();
-    sdlx_destroy_texture(red_circle_texture);
-    sdlx_destroy_texture(green_circle_texture);
-    sdlx_destroy_texture(blue_circle_texture);
+    color_organ_cleanup();
 
     // terminate
     printf("I %s: terminating\n", progname);
@@ -179,9 +184,9 @@ void register_events(void)
 
     // xxx comment
     if (as.state == AUDIO_STATE_IDLE) {
-        loc = sdlx_render_printf(COL2X(0), y_controls, "%s", "DEVICE");
+        loc = sdlx_render_printf(COL2X(0), y_controls, "%s", "dev");
         sdlx_register_event(loc, EVID_DEV);
-        loc = sdlx_render_printf(COL2X(9), y_controls, "%s", "MICROPHONE");
+        loc = sdlx_render_printf(sdlx_win_width-COL2X(3), y_controls, "%s", "mic");
         sdlx_register_event(loc, EVID_MIC);
     } else if (as.state == AUDIO_STATE_RECORD_FROM_MIC ||
                as.state == AUDIO_STATE_RECORD_FROM_DEVICE) {
@@ -218,6 +223,9 @@ void register_events(void)
             y += 1.5*sdlx_char_height_dflt;
         }
     }
+
+    // xxx comment
+    color_organ_register_events();
 
     // register control event to end program
     sdlx_register_control_events(0, NULL,
@@ -261,6 +269,16 @@ void get_file_list(void)
 }
 
 // -----------------  DISPLAY COLOR ORGAN  ---------------------------
+// xxx new file
+
+#define EVID_CO_RED_INCREASE    1000
+#define EVID_CO_RED_DECREASE    1001
+#define EVID_CO_GREEN_INCREASE  1002
+#define EVID_CO_GREEN_DECREASE  1003
+#define EVID_CO_BLUE_INCREASE   1004
+#define EVID_CO_BLUE_DECREASE   1005
+#define EVID_CO_RESET           1006
+#define EVID_CO_PARAMS          1007
 
 #define X_RED_CIRCLE    250
 #define Y_RED_CIRCLE    683
@@ -271,9 +289,88 @@ void get_file_list(void)
 
 #define DECAY  0.01
 
-void proc(sdlx_texture_t *t, int x_ctr, int y_ctr, double newval, double *work, double k)
+#define DEFAULT_K_RED    30
+#define DEFAULT_K_GREEN  50
+#define DEFAULT_K_BLUE   70
+#define DELTA_K          5
+#define MIN_K            0
+#define MAX_K            100
+#define DISP_K_DURATION  100   // cycles
+
+int k_red   = DEFAULT_K_RED;
+int k_green = DEFAULT_K_GREEN;
+int k_blue  = DEFAULT_K_BLUE;
+int disp_k  = 0;
+
+// -------------------------------------------------------------------
+
+void color_organ_init(void)
 {
-    double current, sf;
+    // init color organ red,green,blue circle textures
+    red_circle_texture   = create_circle_texture(COLOR_RED);
+    green_circle_texture = create_circle_texture(COLOR_GREEN);
+    blue_circle_texture  = create_circle_texture(COLOR_BLUE);
+
+    k_red = util_get_numeric_param(data_dir, "k_red", DEFAULT_K_RED);
+    k_green = util_get_numeric_param(data_dir, "k_green", DEFAULT_K_GREEN);
+    k_blue = util_get_numeric_param(data_dir, "k_blue", DEFAULT_K_BLUE);
+}
+
+void color_organ_cleanup(void)
+{
+    // destroy color organ textures
+    sdlx_destroy_texture(red_circle_texture);
+    sdlx_destroy_texture(green_circle_texture);
+    sdlx_destroy_texture(blue_circle_texture);
+}
+
+// -------------------------------------------------------------------
+
+void proc(sdlx_texture_t *t, int x_ctr, int y_ctr, double newval, double *work, int k);
+
+void color_organ_display(sdlx_audio_state_t *as)
+{
+    static double work_red, work_green, work_blue;
+    static int    audio_state_last = AUDIO_STATE_IDLE;
+    static bool   audio_paused_last = false;
+    static bool   first_call = true;
+
+    if (as->state != AUDIO_STATE_IDLE) {
+        proc(red_circle_texture, X_RED_CIRCLE, Y_RED_CIRCLE, as->color_organ.low_band, &work_red, k_red);
+        proc(green_circle_texture, X_GREEN_CIRCLE, Y_GREEN_CIRCLE, as->color_organ.mid_band, &work_green, k_green);
+        proc(blue_circle_texture, X_BLUE_CIRCLE, Y_BLUE_CIRCLE, as->color_organ.high_band, &work_blue, k_blue);
+    }
+
+    if ((as->state != AUDIO_STATE_IDLE) && 
+        (!as->paused) &&
+        (as->state != audio_state_last || as->paused != audio_paused_last))
+    {
+        disp_k = DISP_K_DURATION;
+    }
+    if (first_call) {
+        disp_k = DISP_K_DURATION;
+    }
+    audio_state_last = as->state;
+    audio_paused_last = as->paused;
+    first_call = false;
+    
+    if (disp_k > 0) {
+        sdlx_render_printf_ex2(X_RED_CIRCLE, Y_RED_CIRCLE,
+                               FONT_NORMAL, COLOR_WHITE, FLAG_XY_CTR, WRAP_NONE,
+                               "%d", k_red);
+        sdlx_render_printf_ex2(X_GREEN_CIRCLE, Y_GREEN_CIRCLE,
+                               FONT_NORMAL, COLOR_WHITE, FLAG_XY_CTR, WRAP_NONE,
+                               "%d", k_green);
+        sdlx_render_printf_ex2(X_BLUE_CIRCLE, Y_BLUE_CIRCLE,
+                               FONT_NORMAL, COLOR_WHITE, FLAG_XY_CTR, WRAP_NONE,
+                               "%d", k_blue);
+        disp_k = disp_k - 1;
+    }
+}
+
+void proc(sdlx_texture_t *t, int x_ctr, int y_ctr, double newval, double *work, int k)
+{
+    double current, intensity;
 
     current = *work;
     if (newval > current) {
@@ -284,21 +381,95 @@ void proc(sdlx_texture_t *t, int x_ctr, int y_ctr, double newval, double *work, 
     }
     *work = current;
 
-    sf = k * current;
-    if (sf > 1) sf = 1;
-    if (sf < 0) sf = 0;
+    intensity = (k / 10.0) * current;
+    if (intensity > 1) intensity = 1;
+    if (intensity < 0) intensity = 0;
 
-    sdlx_color_mod_texture(t, sf, sf, sf);
+    sdlx_color_mod_texture(t, intensity, intensity, intensity);
     sdlx_render_texture_ex1(t, x_ctr-250, y_ctr-250, 500, 500);
 }
 
-void display_color_organ(void)
-{
-    static double work_red, work_green, work_blue;
+// -------------------------------------------------------------------
 
-    proc(red_circle_texture, X_RED_CIRCLE, Y_RED_CIRCLE, as.color_organ.low_band, &work_red, 3);
-    proc(green_circle_texture, X_GREEN_CIRCLE, Y_GREEN_CIRCLE, as.color_organ.mid_band, &work_green, 5);
-    proc(blue_circle_texture, X_BLUE_CIRCLE, Y_BLUE_CIRCLE, as.color_organ.high_band, &work_blue, 7);
+void init_loc(sdlx_loc_t *loc, int x, int y, int w, int h);
+
+void color_organ_register_events(void)
+{
+    sdlx_loc_t loc;
+    sdlx_loc_t *locp;
+
+    init_loc(&loc, X_RED_CIRCLE-125, Y_RED_CIRCLE-250, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_RED_INCREASE);
+    init_loc(&loc, X_RED_CIRCLE-125, Y_RED_CIRCLE, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_RED_DECREASE);
+
+    init_loc(&loc, X_GREEN_CIRCLE-125, Y_GREEN_CIRCLE-250, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_GREEN_INCREASE);
+    init_loc(&loc, X_GREEN_CIRCLE-125, Y_GREEN_CIRCLE, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_GREEN_DECREASE);
+
+    init_loc(&loc, X_BLUE_CIRCLE-125, Y_BLUE_CIRCLE-250, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_BLUE_INCREASE);
+    init_loc(&loc, X_BLUE_CIRCLE-125, Y_BLUE_CIRCLE, 250, 250);
+    sdlx_register_event(&loc, EVID_CO_BLUE_DECREASE);
+
+    locp = sdlx_render_printf(sdlx_win_width-COL2X(3), ROW2Y(0.5), "%s", "RST");
+    sdlx_register_event(locp, EVID_CO_RESET);
+
+    locp = sdlx_render_printf(sdlx_win_width-COL2X(3), ROW2Y(2.5), "%s", "PRM");
+    sdlx_register_event(locp, EVID_CO_PARAMS);
+}
+
+void color_organ_process_event(sdlx_event_t *ev)
+{
+    // xxx only if active ?
+
+    switch (ev->event_id) {
+    case EVID_CO_RED_INCREASE:
+    case EVID_CO_RED_DECREASE:
+        k_red += (ev->event_id == EVID_CO_RED_INCREASE ? DELTA_K : - DELTA_K);
+        if (k_red > MAX_K) k_red = MAX_K;
+        if (k_red < MIN_K) k_red = MIN_K;
+        disp_k = DISP_K_DURATION;
+        util_set_numeric_param(data_dir, "k_red", k_red);
+        break;
+    case EVID_CO_GREEN_INCREASE:
+    case EVID_CO_GREEN_DECREASE:
+        k_green += (ev->event_id == EVID_CO_GREEN_INCREASE ? DELTA_K : - DELTA_K);
+        if (k_green > MAX_K) k_green = MAX_K;
+        if (k_green < MIN_K) k_green = MIN_K;
+        disp_k = DISP_K_DURATION;
+        util_set_numeric_param(data_dir, "k_green", k_green);
+        break;
+    case EVID_CO_BLUE_INCREASE:
+    case EVID_CO_BLUE_DECREASE:
+        k_blue += (ev->event_id == EVID_CO_BLUE_INCREASE ? DELTA_K : - DELTA_K);
+        if (k_blue > MAX_K) k_blue = MAX_K;
+        if (k_blue < MIN_K) k_blue = MIN_K;
+        disp_k = DISP_K_DURATION;
+        util_set_numeric_param(data_dir, "k_blue", k_blue);
+        break;
+    case EVID_CO_RESET:
+        k_red   = DEFAULT_K_RED;
+        k_green = DEFAULT_K_GREEN;
+        k_blue  = DEFAULT_K_BLUE;
+        disp_k  = DISP_K_DURATION;
+        util_set_numeric_param(data_dir, "k_red", k_red);
+        util_set_numeric_param(data_dir, "k_green", k_green);
+        util_set_numeric_param(data_dir, "k_blue", k_blue);
+        break;
+    case EVID_CO_PARAMS:
+        disp_k  = DISP_K_DURATION;
+        break;
+    }
+}
+
+void init_loc(sdlx_loc_t *loc, int x, int y, int w, int h)
+{
+    loc->x = x;
+    loc->y = y;
+    loc->w = w;
+    loc->h = h;
 }
 
 // -----------------  UTILS  -----------------------------------------
