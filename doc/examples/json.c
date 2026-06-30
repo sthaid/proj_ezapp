649a0d74a24994304ec321e2039291072ded702a
Steven Haid <stevenhaid@gmail.com>
2026-06-30 12:05:07 -0400

    add examples

---
+++ doc/examples/json.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils.h>
+
+void print_json_value(json_value_t *x);
+
+int main()
+{
+    char *json_text;
+    void *json_root;
+
+    // read and parse file example.json
+    json_text = util_read_file(".", "example.json", NULL);
+    json_root = util_json_parse(json_text, NULL);
+
+    // print fields, remember to supply NULL as the last arg
+    print_json_value(util_json_get_value(json_root, "type", NULL));
+    print_json_value(util_json_get_value(json_root, "properties", "units", NULL));
+
+    // print fields from periods array [0]
+    print_json_value(util_json_get_value(json_root, "properties", "periods", "0", "number", NULL));
+    print_json_value(util_json_get_value(json_root, "properties", "periods", "0", "string", NULL));
+
+    // print fields from periods array [1], illustrating a different approach than above
+    json_value_t *jv = util_json_get_value(json_root, "properties", "periods", "1", NULL);
+    void *period1 = jv->u.object;
+    print_json_value(util_json_get_value(period1, "number", NULL));
+    print_json_value(util_json_get_value(period1, "string", NULL));
+
+    // cleanup
+    util_json_free(json_root);
+    free(json_text);
+    return 0;
+}
+
+void print_json_value(json_value_t *jv)
+{
+    if (jv == NULL) {
+        printf("ERROR: json_value is NULL\n");
+        exit(1);
+    }
+
+    switch (jv->type) {
+    case JSON_TYPE_FLAG:   printf("FLAG   %d\n", jv->u.flag);   break;
+    case JSON_TYPE_NUMBER: printf("NUMBER %g\n", jv->u.number); break;
+    case JSON_TYPE_STRING: printf("STRING %s\n", jv->u.string); break;
+    case JSON_TYPE_OBJECT: printf("OBJECT %p\n", jv->u.object); break;
+    default:
+        printf("ERROR: bad json_value type %d\n", jv->type);
+        exit(1);
+    }
+}
